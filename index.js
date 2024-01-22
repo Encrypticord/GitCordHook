ebd519ca1b06c4c2210646805d2fde7e0105dbc5
Carter L. SD-55 <longcarter10@gmail.com>
2024-01-22 20:30:42 +0100

    Add Correct Copyright Year

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  *
  * Encrypticord GitCordHook - A GitHub & Discord Webhook Bridge
  *
- * Copyright © 2023 Encrypticord Services All rights reserved.
+ * Copyright © 2024 Encrypticord Services All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
